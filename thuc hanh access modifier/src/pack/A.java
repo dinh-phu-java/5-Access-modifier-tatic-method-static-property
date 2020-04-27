06f5204c5b86f2f0e61f73cb0a9811cfd9556f3c
Dinh Phu <ndp17hp@gmail.com>
2020-04-27 21:47:24 +0700

    them file

---
+++ thuc hanh access modifier/src/pack/A.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class A {
+    public void msg(){
+        System.out.println("Hello");
+    }
+    protected void msg_protected(){
+        System.out.println("Hello protected");
+    }
+}
